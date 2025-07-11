5a1cc09cd9bc1943ab3ac0604df507eb94dd1b69
pingzhili <pingzhili@Pingzhis-MacBook-Pro.local>
2025-07-11 02:13:00 -0700

    update

--- experiment-scripts/bashline.sh
+++ experiment-scripts/bashline.sh
@@ -3,24 +3,25 @@ export WANDB_API_KEY=2b60f655a687ad1161d31f0002256865e1ace428
 export WANDB_PROJECT=llm-reasoning
 export VLLM_WORKER_MULTIPROC_METHOD=spawn # Required for vLLM
 
-MODEL=Qwen/Qwen2.5-32B
+MODEL=Qwen/Qwen3-32B
+EXP_NAME="qwen2.5-32b-base"
 MODEL_ARGS="model_name=$MODEL,dtype=bfloat16,max_model_length=32768,gpu_memory_utilization=0.8,generation_parameters={max_new_tokens:32768,temperature:0.6,top_p:0.95},use_chat_template=true,tensor_parallel_size=2"
 OUTPUT_DIR=data/evals/$MODEL
 
 # AIME 2024
 TASK=aime24
-WANDB_NAME="$MODEL-$TASK"
+WANDB_NAME="$EXP_NAME-$TASK"
 lighteval vllm $MODEL_ARGS "lighteval|$TASK|0|0" \
-    --output-dir $OUTPUT_DIR --save-details --wandb 2>&1 | tee $MODEL_$TASK_output.log
+    --output-dir $OUTPUT_DIR --save-details --wandb 2>&1 | tee $WANDB_NAME_$TASK_output.log
 
 # MATH
 TASK=math_500
-WANDB_NAME="$MODEL-$TASK"
+WANDB_NAME="$EXP_NAME-$TASK"
 lighteval vllm $MODEL_ARGS "lighteval|$TASK|0|0" \
-    --output-dir $OUTPUT_DIR --save-details --wandb 2>&1 | tee $MODEL_$TASK_output.log
+    --output-dir $OUTPUT_DIR --save-details --wandb 2>&1 | tee $WANDB_NAME_$TASK_output.log
 
 # GPQA Diamond
 TASK=gsm8k
-WANDB_NAME="$MODEL-$TASK"
+WANDB_NAME="$EXP_NAME-$TASK"
 lighteval vllm $MODEL_ARGS "lighteval|$TASK|0|0" \
-    --output-dir $OUTPUT_DIR --save-details --wandb 2>&1 | tee $MODEL_$TASK_output.log
+    --output-dir $OUTPUT_DIR --save-details --wandb 2>&1 | tee $WANDB_NAME_$TASK_output.log
