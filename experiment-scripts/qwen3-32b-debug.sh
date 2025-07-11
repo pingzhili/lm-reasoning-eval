d6621f31aaf51a67623625b95b96f0f0ba8f084a
pingzhili <pingzhili@Pingzhis-MacBook-Pro.local>
2025-07-11 15:35:17 -0700

    update

--- experiment-scripts/qwen3-32b-debug.sh
+++ experiment-scripts/qwen3-32b-debug.sh
@@ -6,19 +6,21 @@ export VLLM_WORKER_MULTIPROC_METHOD=spawn # Required for vLLM
 MODEL=Qwen/Qwen3-32B
 MODEL_ARGS="model_name=$MODEL,dtype=bfloat16,max_model_length=32768,gpu_memory_utilization=0.8,generation_parameters={max_new_tokens:32768,temperature:0.6,top_p:0.95,returns_logits:true},use_chat_template=true,tensor_parallel_size=2,enable_thinking=false"
 OUTPUT_DIR=data/evals/$MODEL-nothinking-debug
-mkdir $OUTPUT_DIR/$TASK # for logging
 
 # AIME 2024
 TASK=aime24
+mkdir -p $OUTPUT_DIR/$TASK # for logging
 lighteval vllm $MODEL_ARGS "lighteval|$TASK|0|0" \
     --output-dir $OUTPUT_DIR --save-details --wandb --max-samples 4 2>&1 | tee $OUTPUT_DIR/$TASK/output.log
 
 # MATH
 TASK=math_500
+mkdir -p $OUTPUT_DIR/$TASK # for logging
 lighteval vllm $MODEL_ARGS "lighteval|$TASK|0|0" \
     --output-dir $OUTPUT_DIR --save-details --wandb --max-samples 4 2>&1 | tee $OUTPUT_DIR/$TASK/output.log
 
 # GPQA Diamond
 TASK=gsm8k
+mkdir -p $OUTPUT_DIR/$TASK # for logging
 lighteval vllm $MODEL_ARGS "lighteval|$TASK|0|0" \
     --output-dir $OUTPUT_DIR --save-details --wandb --max-samples 4 2>&1 | tee $OUTPUT_DIR/$TASK/output.log
