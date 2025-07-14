8c80cbd6a973f9dd6c2088d78dc7b89c96b8ecfe
pingzhili <pingzhili@Pingzhis-MacBook-Pro.local>
2025-07-14 02:28:30 -0700

    update

--- experiment-scripts/qwen3-32b-non-thinking.sh
+++ experiment-scripts/qwen3-32b-non-thinking.sh
@@ -6,6 +6,7 @@ export VLLM_WORKER_MULTIPROC_METHOD=spawn # Required for vLLM
 MODEL=Qwen/Qwen3-32B
 MODEL_ARGS="model_name=$MODEL,dtype=bfloat16,max_model_length=32768,gpu_memory_utilization=0.8,generation_parameters={max_new_tokens:32768,temperature:0.6,top_p:0.95,returns_logits:false},use_chat_template=true,tensor_parallel_size=2,enable_thinking=false"
 OUTPUT_DIR=data/evals/$MODEL-nothinking
+mkdir -p $OUTPUT_DIR
 
 # AIME 2024
 TASK=aime24
