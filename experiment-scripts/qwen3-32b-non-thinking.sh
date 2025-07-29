4ced73bbba5908470cdf2f80a13d345c9fc87d56
pingzhili <pingzhili@Pingzhis-MacBook-Pro.local>
2025-07-28 21:25:37 -0700

    update

--- experiment-scripts/qwen3-32b-non-thinking.sh
+++ experiment-scripts/qwen3-32b-non-thinking.sh
@@ -1,10 +1,10 @@
-export CUDA_VISIBLE_DEVICES=1
 export WANDB_API_KEY=2b60f655a687ad1161d31f0002256865e1ace428
 export WANDB_PROJECT=llm-reasoning
 export VLLM_WORKER_MULTIPROC_METHOD=spawn # Required for vLLM
 
+NUM_GPUS=$(python -c "import torch; print(torch.cuda.device_count())")
 MODEL=Qwen/Qwen3-32B
-MODEL_ARGS="model_name=$MODEL,dtype=bfloat16,max_model_length=32768,gpu_memory_utilization=0.8,generation_parameters={max_new_tokens:32768,temperature:0.6,top_p:0.95,returns_logits:false},use_chat_template=true,tensor_parallel_size=2,enable_thinking=false"
+MODEL_ARGS="model_name=$MODEL,dtype=bfloat16,max_model_length=32768,gpu_memory_utilization=0.8,generation_parameters={max_new_tokens:32768,temperature:0.6,top_p:0.95,returns_logits:false},use_chat_template=true,tensor_parallel_size=$NUM_GPUS,enable_thinking=false"
 OUTPUT_DIR=data/evals/$MODEL-nothinking
 mkdir -p $OUTPUT_DIR
 
