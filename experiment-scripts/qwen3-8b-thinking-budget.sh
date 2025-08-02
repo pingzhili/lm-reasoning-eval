da584cdfc8f8cc8692bf883f1acb579a0b3f5ea6
pingzhili <pingzhili@Pingzhis-MacBook-Pro.local>
2025-08-02 08:14:25 -0700

    update

--- experiment-scripts/qwen3-8b-thinking-budget.sh
+++ experiment-scripts/qwen3-8b-thinking-budget.sh
@@ -11,25 +11,25 @@ OUTPUT_DIR=data/evals/$MODEL-thinking-budget-$THINKING_BUDGET
 mkdir -p $OUTPUT_DIR
 
 
-# AIME 2024
-TASK=aime24
-lighteval vllm $MODEL_ARGS "lighteval|$TASK|0|0" \
-    --output-dir $OUTPUT_DIR --save-details 2>&1 | tee "logs/log_$(date +%Y%m%d_%H%M%S)_${RANDOM}.log"
-
-# AIME 2025
-TASK=aime25
-lighteval vllm $MODEL_ARGS "lighteval|$TASK|0|0" \
-    --output-dir $OUTPUT_DIR --save-details 2>&1 | tee "logs/log_$(date +%Y%m%d_%H%M%S)_${RANDOM}.log"
+## AIME 2024
+#TASK=aime24
+#lighteval vllm $MODEL_ARGS "lighteval|$TASK|0|0" \
+#    --output-dir $OUTPUT_DIR --save-details 2>&1 | tee "logs/log_$(date +%Y%m%d_%H%M%S)_${RANDOM}.log"
+#
+## AIME 2025
+#TASK=aime25
+#lighteval vllm $MODEL_ARGS "lighteval|$TASK|0|0" \
+#    --output-dir $OUTPUT_DIR --save-details 2>&1 | tee "logs/log_$(date +%Y%m%d_%H%M%S)_${RANDOM}.log"
 
 # MATH
 TASK=math_500
 lighteval vllm $MODEL_ARGS "lighteval|$TASK|0|0" \
     --output-dir $OUTPUT_DIR --save-details 2>&1 | tee "logs/log_$(date +%Y%m%d_%H%M%S)_${RANDOM}.log"
 
-# GSM8K
-TASK=gsm8k
-lighteval vllm $MODEL_ARGS "lighteval|$TASK|0|0" \
-    --output-dir $OUTPUT_DIR --save-details 2>&1 | tee "logs/log_$(date +%Y%m%d_%H%M%S)_${RANDOM}.log"
+## GSM8K
+#TASK=gsm8k
+#lighteval vllm $MODEL_ARGS "lighteval|$TASK|0|0" \
+#    --output-dir $OUTPUT_DIR --save-details 2>&1 | tee "logs/log_$(date +%Y%m%d_%H%M%S)_${RANDOM}.log"
 
 # GPQA Diamond
 TASK=gpqa:diamond
