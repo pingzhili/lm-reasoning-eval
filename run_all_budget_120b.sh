b10904d4a96415fe6fe631217705a37a23cd8562
pingzhili <pingzhili@Pingzhis-MacBook-Pro.local>
2025-08-07 01:37:34 -0700

    update

--- run_all_budget_120b.sh
+++ run_all_budget_120b.sh
@@ -7,3 +7,15 @@ sleep 60
 CUDA_VISIBLE_DEVICES=3 bash experiment-scripts/gptoss-120b-thinking.sh low 8003 &
 CUDA_VISIBLE_DEVICES=4 bash experiment-scripts/gptoss-120b-thinking.sh medium 8004 &
 CUDA_VISIBLE_DEVICES=5 bash experiment-scripts/gptoss-120b-thinking.sh high 8005 &
+
+wait
+
+CUDA_VISIBLE_DEVICES=0 bash experiment-scripts/gptoss-120b-thinking.sh low 8000 &
+CUDA_VISIBLE_DEVICES=1 bash experiment-scripts/gptoss-120b-thinking.sh medium 8001 &
+CUDA_VISIBLE_DEVICES=2 bash experiment-scripts/gptoss-120b-thinking.sh high 8002 &
+
+sleep 60
+
+CUDA_VISIBLE_DEVICES=3 bash experiment-scripts/gptoss-120b-thinking.sh low 8003 &
+CUDA_VISIBLE_DEVICES=4 bash experiment-scripts/gptoss-120b-thinking.sh medium 8004 &
+CUDA_VISIBLE_DEVICES=5 bash experiment-scripts/gptoss-120b-thinking.sh high 8005 &
