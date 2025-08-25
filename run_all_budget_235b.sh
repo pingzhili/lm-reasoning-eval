ed2ca94ea926b1a270601fe09edc8538e843a715
pingzhili <pingzhili@Pingzhis-MacBook-Pro.local>
2025-08-25 15:03:27 -0400

    update

--- run_all_budget_235b.sh
+++ run_all_budget_235b.sh
@@ -1,6 +1,6 @@
 for gpu in {0..3}; do
     budget=$((2048 + gpu * 2048))
-    CUDA_VISIBLE_DEVICES=$((gpu * 2)), $((gpu * 2 + 1)) bash experiment-scripts/qwen3-30b-thinking-budget.sh $budget &
+    CUDA_VISIBLE_DEVICES=$((gpu * 2)), $((gpu * 2 + 1)) bash experiment-scripts/qwen3-235b-thinking-budget.sh $budget &
     echo "Started GPU $gpu with budget $budget"
 done
 
