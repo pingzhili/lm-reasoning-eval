11e20b09e5f01f4816c450395a940d90d003330c
pingzhili <pingzhili@Pingzhis-MacBook-Pro.local>
2025-09-15 16:30:23 -0400

    update

--- experiment-scripts/qwen3-235b-thinking.sh
+++ experiment-scripts/qwen3-235b-thinking.sh
@@ -8,10 +8,10 @@ MODEL_ARGS="model_name=$MODEL,max_model_length=32768,gpu_memory_utilization=0.8,
 OUTPUT_DIR=data/evals/$MODEL-thinking
 mkdir -p $OUTPUT_DIR
 
-# AIME 2024
-TASK=aime24
-lighteval vllm $MODEL_ARGS "lighteval|$TASK|0|0" \
-    --output-dir $OUTPUT_DIR --save-details  2>&1 | tee "logs/log_$(date +%Y%m%d_%H%M%S)_${RANDOM}.log"
+## AIME 2024
+#TASK=aime24
+#lighteval vllm $MODEL_ARGS "lighteval|$TASK|0|0" \
+#    --output-dir $OUTPUT_DIR --save-details  2>&1 | tee "logs/log_$(date +%Y%m%d_%H%M%S)_${RANDOM}.log"
 
 # AIME 2025
 TASK=aime25
