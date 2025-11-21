e921b9b54cd0338f0e1a628168f21415f049a335
Pingzhi Li <pingzhili@Pingzhis-MacBook-Pro.local>
2025-11-21 03:18:09 -0500

    update

---
+++ experiment-scripts/re/qwen3-8b-self-judge-lcb.sh
@@ -0,0 +1,19 @@
+export WANDB_API_KEY=2b60f655a687ad1161d31f0002256865e1ace428
+export WANDB_PROJECT=llm-reasoning
+export VLLM_WORKER_MULTIPROC_METHOD=spawn # Required for vLLM
+
+NUM_GPUS=$(python -c "import torch; print(torch.cuda.device_count())")
+MODEL=Qwen/Qwen3-8B
+MODEL_ARGS="model_name=$MODEL,dtype=bfloat16,max_model_length=32768,gpu_memory_utilization=0.8,generation_parameters={max_new_tokens:32768,temperature:0.6,top_p:0.95,returns_logits:false},use_chat_template=true,tensor_parallel_size=2,tensor_parallel_size=$NUM_GPUS,enable_thinking=true,self_judge_thinking=true"
+OUTPUT_DIR=/mnt/task_wrapper/user_output/artifacts/$MODEL-self-judge
+mkdir -p $OUTPUT_DIR
+
+# LCB code gen
+TASK=lcb:codegeneration
+lighteval vllm $MODEL_ARGS "extended|$TASK|0|0" \
+    --output-dir $OUTPUT_DIR --save-details 2>&1 | tee "logs/log_$(date +%m%d_%H%M%S)_$TASK.log"
+
+## HumanEval
+#TASK=humaneval
+#lighteval vllm $MODEL_ARGS "helm|$TASK|0|0" \
+#    --output-dir $OUTPUT_DIR --save-details 2>&1 | tee "logs/log_$(date +%m%d_%H%M%S)_$TASK.log"
